20f386fc2095bdc8b0daeb65d4027579b3da7eae
kapelnik <tal.kapelnik@gmail.com>
2020-08-15 18:02:11 +0300

    working gui on stateM

--- src/gui.erl
+++ src/gui.erl
@@ -23,21 +23,27 @@ init() ->do_init().%%wx:batch(fun() -> do_init() end).
           frame,
           env,
           panel,
+          text,
+          canvas,
           bitmap,
           overlay,
           pos
         }).
 do_init() ->    %%F=frame P=panel C=canvas
-  wx:new(),
-  F = wxFrame:new(wx:null(), -1, "B.A.T.M.A.N Display", [{size, {?Width,?Height}}]),
-  P = wxPanel:new(F, [{size, {?Width,?Height-100}}, {pos,{500, 500}}]),
-  C = wxPanel:new(P, [{style, ?wxFULL_REPAINT_ON_RESIZE}]),
-  MainSizer = wxBoxSizer:new(?wxVERTICAL),
-  Sizer = wxStaticBoxSizer:new(?wxVERTICAL, P, [{label, "Batman bounderies"}]),
-  T = wxStaticText:new(P, -1, "Displaying..",[]),
-  B = wxButton:new(P, 0, [{label, "Start"}, {size, {150, 50}}]),
-  wxButton:connect(B, command_button_clicked, [{callback, fun handle_click/2}, {userData, #{text => T, env => wx:get_env()}}]),
+  Env = wx:new(),     %%create a wx environment
+  F = wxFrame:new(wx:null(), -1, "B.A.T.M.A.N Display", [{size, {?Width,?Height}}]),  %Creates the main frame for the gui
+  P = wxPanel:new(F, [{size, {?Width,?Height-100}}]), % a panel we will split with sizers
+  C = wxPanel:new(P, [{style, ?wxFULL_REPAINT_ON_RESIZE}]), %the main canvas to print on the points
+  MainSizer = wxBoxSizer:new(?wxVERTICAL),      %main sizer for alignment within the panel
+  Sizer = wxStaticBoxSizer:new(?wxVERTICAL, P, [{label, "Batman bounderies"}]), %inside frame for batman protocol display
+  T = wxStaticText:new(P, -1, "Click to start",[]),
 
+  %create a botton to initiate batman protocol and connect it to its event handler
+  B = wxButton:new(P, 0, [{label, "Start"}, {size, {150, 50}}]),
+  wxButton:connect(B, command_button_clicked, [{callback, fun handle_click/2}, {userData, #{canvas => C,text => T, env => wx:get_env()}}]),
+
+  %Connect the Panel to paint event
+  wxPanel:connect(P, paint, [callback]),
   wxSizer:add(Sizer, B, [{border, 5}, {flag, ?wxALL}]),
   wxSizer:addSpacer(Sizer, 5),
   wxSizer:add(Sizer, T, [{border, 5}, {flag, ?wxALL}]),
@@ -46,19 +52,52 @@ do_init() ->    %%F=frame P=panel C=canvas
   wxSizer:add(MainSizer, Sizer, [{flag, ?wxEXPAND}, {proportion, 1}]),
   wxPanel:setSizer(P, MainSizer),
   wxSizer:layout(MainSizer),
-%%  {W,H} = wxPanel:getSize(P),
-%%  Bitmap = wxBitmap:new(erlang:max(W,30),erlang:max(30,H)),
+
+  %Frame is ready for display
+
+  initcanvas(C),
   wxFrame:show(F),
+  mainLoop(#state{canvas = C,text = T, env = Env}).
+
 %%c(gui). gui:init().
-%%
-  mainLoop(#state{frame = F, env = wx:get_env(),panel = P}).
+%%c(gui). gui:init().
+%%c(gui). gui:init().
+%%c(gui). gui:init().
+%%c(gui). gui:init().
 
-handle_click(#wx{obj = _, userData = #{text := T, env := Env}},_Event) ->
+handle_click(S=#wx{obj = _, userData = #{canvas := C,text := T, env := Env}},_Event) ->
   wx:set_env(Env),
-  wxStaticText:setLabel(T, "clicked").
+  wxStaticText:setLabel(T, "Running..").
 
-mainLoop(#state{env = Env, frame = F , panel = P}) -> okay.
+mainLoop(S=#state{ canvas= C ,text = T, env = Env}) ->
 
+  DC = wxPaintDC:new(C),
+  X=rand:uniform(10),
+  wxDC:clear(DC),
+  wxDC:setBrush(DC, ?wxTRANSPARENT_BRUSH),
+  wxDC:setPen(DC, wxPen:new(?wxBLACK, [{width, 2}])),
+  wxDC:drawCircle(DC, {200+X, 200-X}, 3),
+  wxDC:drawCircle(DC, {1+X, 2-X}, 3),
+  wxPaintDC:destroy(DC),
+  receive
+    after 16->mainLoop(S)
+  end.
+
+
+
+initcanvas(C) ->
+  %%
+  DC = wxPaintDC:new(C),
+  Image = wxImage:new("image.jpg"),
+  Image2 = wxImage:scale(Image, wxImage:getWidth(Image) div 3,
+  wxImage:getHeight(Image) div 3),
+  Bmp = wxBitmap:new(Image2),
+  wxImage:destroy(Image),
+  wxImage:destroy(Image2),
+
+  wxDC:clear(DC),
+  wxBitmap:destroy(Bmp),
+  wxPaintDC:destroy(DC).
 
 
 
